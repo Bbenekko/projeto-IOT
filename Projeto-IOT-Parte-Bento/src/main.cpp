52b345ace21a9fa975c80ee418b492c9e06bbde8
Bbenekko <bbeneko@outlook.com>
2025-11-05 14:13:23 -0300

    Adição da biblioteca CCS e calibração do mesmo

--- Projeto-IOT-Parte-Bento/src/main.cpp
+++ Projeto-IOT-Parte-Bento/src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
 #include <Adafruit_BME680.h>
+#include <Adafruit_CCS811.h>
 
 Adafruit_BME680 sensorBME;
+Adafruit_CCS811 sensorCCS;
+
+/*
+
+
+TODO: o sensor particular (DSM 501A) não possui biblioteca, então terá que ser usado de maneira "crua"
+
+
+*/
 
 void setup() 
 {
@@ -14,15 +24,32 @@ void setup()
     while (true); 
   } 
 
+  if (!sensorCCS.begin()) 
+  { 
+    Serial.println("Erro no sensor CCS"); 
+    while (true); 
+  } 
+
+  // Calibrando BME
   // aumenta amostragem dos sensores (1X, 2X, 4X, 8X, 16X ou NONE) 
   sensorBME.setTemperatureOversampling(BME680_OS_8X); 
   sensorBME.setHumidityOversampling(BME680_OS_2X); 
+
+  // Calibrando CCS
+  float temp = sensorCCS.calculateTemperature();
+  sensorCCS.setTempOffset(temp - 25.0);
 
 }
 
 void loop() 
 {
   sensorBME.performReading();
+  
+  if (sensorCCS.available())
+  {
+    float eCO2 = sensorCCS.geteCO2();
+    float TVOC = sensorCCS.getTVOC();
+  }
 
   float temperatura = sensorBME.temperature; 
   float umidade = sensorBME.humidity;
